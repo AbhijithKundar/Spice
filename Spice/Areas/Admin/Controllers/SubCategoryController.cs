9d47a8d5025c4d9f48df590288ef60dd2032c0f9
AbhijithKundar <abhijith.kundar@gmail.com>
2019-09-05 20:01:55 +0530

    Category Sub Category

---
+++ Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models;
+using Spice.Models.ViewModel;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET
+        public async Task<IActionResult> Index()
+        {
+            var subCategory = await _db.SubCategory.Include(x => x.Category).ToListAsync();
+            return View(subCategory);
+        }
+
+        //Get - CREATE
+        public async Task<IActionResult> Create()
+        {
+            SubcategoryAndCategoryViewModel model = new SubcategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                Subcategory = new Models.SubCategory(),
+                SubCategoryList = await _db.SubCategory.OrderBy(x => x.Name).Select(x => x.Name).Distinct().ToListAsync()
+            };
+
+            return View(model);
+
+        }
+
+        //POSt - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SubcategoryAndCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var SubCategoryExists = _db.SubCategory.Include(x => x.Category).Where(x => x.Name == model.Subcategory.Name && x.CategoryId == model.Subcategory.CategoryId);
+
+                if(SubCategoryExists.Count() > 0)
+                {
+
+                    StatusMessage = $"Error: Sub Category Exists Under {SubCategoryExists.First().Category.Name}. Please use another name";
+                }
+                else
+                {
+                    _db.SubCategory.Add(model.Subcategory);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+
+                }
+            }
+
+            SubcategoryAndCategoryViewModel modelVM = new SubcategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                Subcategory =model.Subcategory,
+                SubCategoryList = await _db.SubCategory.OrderBy(x => x.Name).Select(x => x.Name).Distinct().ToListAsync(),
+                StatusMessage = StatusMessage
+            };
+
+            return View(modelVM);
+
+        }
+
+
+
+
+
+        [ActionName("GetSubCategory")]
+        public async Task<IActionResult> GetSubCategory(int id)
+        {
+
+            List<SubCategory> subCategories = new List<SubCategory>();
+
+            subCategories =await _db.SubCategory.Where(x => x.CategoryId == id).ToListAsync();
+
+            return Json(new SelectList(subCategories, "Id", "Name"));
+
+        }
+
+    }
+}
---
+++ Spice/Extension/IEnumerableExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Extension
+{
+    public static class IEnumerableExtension
+    {
+
+        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this IEnumerable<T> items, int selectedValue)
+        {
+
+            return items.Select(x => new SelectListItem
+            {
+                Text = x.GetPropertyValue("Name"),
+                Value = x.GetPropertyValue("Id"),
+                Selected = x.GetPropertyValue("Id").Equals(selectedValue.ToString())
+
+            });
+
+        }
+    }
+}
